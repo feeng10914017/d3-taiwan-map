cdebdb38c9332c78e87e5e4a5f99fca7f70f4fb9
feeng10914017 <feeng10914017@gmail.com>
2024-08-29 15:57:20 +0800

    build: add system config (tailwind CSS and Prettier)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,7 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
+    <h1 class="bg-slate-200 text-orange-400">Welcome to {{ title }}!</h1>
     <router-outlet />
   `,
   styles: [],
