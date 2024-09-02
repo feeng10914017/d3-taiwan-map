3e36144eefed5622319f1e85ee88cce5129e60f7
feeng10914017 <feeng10914017@gmail.com>
2024-09-02 20:26:42 +0800

    build: add system config (Prettier and D3)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TaiwanMapComponent } from './components/taiwan-map/taiwan-map.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TaiwanMapComponent],
   template: `
-    <h1 class="bg-slate-200 text-orange-400">Welcome to {{ title }}!</h1>
+    <h1 class="sticky top-0 p-5 text-4xl shadow-lg">Welcome to {{ title }}!</h1>
+    <app-taiwan-map class="h-full grow bg-sky-200" />
+
     <router-outlet />
   `,
   styles: [],
+  host: { class: 'flex flex-col min-h-dvh' },
 })
 export class AppComponent {
   title = 'D3TaiwanMap';
